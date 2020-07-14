f097308fc3f35e9d1e1bf5063189f4084cc4d96b
unknown <guy.bartkus@gmail.com>
2020-07-14 10:12:50 -0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const settings = require('./settings');
+require('colors');
+
+if(!fs.existsSync('./ytapi-auth.json')) {
+    fs.writeFileSync('./ytapi-auth.json', "");
+    console.error("You need to add the contents of your YouTube API service account key json file to ytapi-auth.json.");
+    return;
+}
+
+if(!fs.existsSync('./ytdl-auth.json')) {
+    fs.writeFileSync('./ytdl-auth.json', JSON.stringify({username: "", password: ""}, null, 2));
+    console.error("Warning: No credentials specified in ytdl-auth.json, will not be able to download age restricted videos.".yellow);
+}
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "./ytapi-auth.json";
---
+++ settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    videos: [
+        {folder: "./playlist1", url: "https://www.youtube.com/someneetplaylisturl"}
+    ]
+}
+
+//
+module.exports = {
+    urls: [
+        {
+            folder: "./playlist1", 
+            url: "https://www.youtube.com/playlist?list=PLACtoIqxCvGiGqqvzZt-a2nmprRa8P-Dy"
+        }
+    ],
+    audioonly: false,
+    playlist: "--yes-playlist" //--no-playlist
+}
