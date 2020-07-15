9783a9d28a47e78561aa5bec0802eea186d33c86
dereckdemezquita <dereck@demezquita.com>
2020-07-15 11:07:53 +0200

    Default user settings.

---
+++ inputs/user-settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    urls: [
+        {
+            name: "Playlist1",
+            link: "https://www.youtube.com/playlist?list=PLACtoIqxCvGiGqqvzZt-a2nmprRa8P-Dy"
+        }
+    ],
+    audio_only: true,
+    out_plname: true,
+    descriptions: true,
+    thumbnails: true
+}
